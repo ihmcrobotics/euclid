01030b7e830bf2034a4468ffb59af2468cc187c9
Carson Wilber <cwilber@ihmc.us>
2017-10-14 16:43:10 -0500

    final keyword, clean up imports, remove unnecessary EPSILON

--- test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
+++ test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
@@ -2,8 +2,6 @@ package us.ihmc.euclid.referenceFrame;
 
 public final class FrameQuaternionTest extends FrameQuaternionReadOnlyTest<FrameQuaternion>
 {
-   public static final double EPSILON = 1e-10;
-
    @Override
    public FrameQuaternion createTuple(ReferenceFrame referenceFrame, double x, double y, double z, double s)
    {
