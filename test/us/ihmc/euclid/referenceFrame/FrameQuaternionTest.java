de7ee6d1e4464d28f916525240351d94c09b6a6a
Carson Wilber <cwilber@ihmc.us>
2017-10-14 16:42:45 -0500

    final keyword added to FrameQuaternionTest

--- test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
+++ test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
@@ -1,22 +1,6 @@
 package us.ihmc.euclid.referenceFrame;
 
-import org.ejml.data.DenseMatrix64F;
-import org.ejml.ops.RandomMatrices;
-import org.junit.Assert;
-import org.junit.Test;
-import us.ihmc.euclid.interfaces.GeometryObject;
-import us.ihmc.euclid.referenceFrame.tools.EuclidFrameRandomTools;
-import us.ihmc.euclid.tools.EuclidCoreRandomTools;
-import us.ihmc.euclid.tools.EuclidCoreTestTools;
-import us.ihmc.euclid.tuple4D.Quaternion;
-import us.ihmc.euclid.tuple4D.interfaces.QuaternionBasics;
-import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
-
-import java.util.Random;
-
-import static org.junit.Assert.assertEquals;
-
-public class FrameQuaternionTest extends FrameQuaternionReadOnlyTest<FrameQuaternion>
+public final class FrameQuaternionTest extends FrameQuaternionReadOnlyTest<FrameQuaternion>
 {
    public static final double EPSILON = 1e-10;
 
