67f88270dae46a64aa906564f53bdc4d3f868fce
Carson Wilber <cwilber@ihmc.us>
2017-10-26 15:11:28 -0500

    testQuaternionBasicsFeatures and testFrameGeometryObjectFeatures

--- test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
+++ test/us/ihmc/euclid/referenceFrame/FrameQuaternionTest.java
@@ -1,21 +1,20 @@
 package us.ihmc.euclid.referenceFrame;
 
 import org.junit.Test;
-import us.ihmc.euclid.axisAngle.interfaces.AxisAngleReadOnly;
-import us.ihmc.euclid.matrix.interfaces.Matrix3DReadOnly;
-import us.ihmc.euclid.matrix.interfaces.RotationMatrixReadOnly;
 import us.ihmc.euclid.referenceFrame.exceptions.ReferenceFrameMismatchException;
 import us.ihmc.euclid.referenceFrame.interfaces.ReferenceFrameHolder;
 import us.ihmc.euclid.referenceFrame.tools.EuclidFrameAPITestTools;
 import us.ihmc.euclid.referenceFrame.tools.EuclidFrameRandomTools;
-import us.ihmc.euclid.tuple2D.interfaces.Tuple2DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.euclid.tuple4D.QuaternionBasicsTest;
 import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
-import us.ihmc.euclid.tuple4D.interfaces.Tuple4DReadOnly;
-import us.ihmc.euclid.tuple4D.interfaces.Vector4DBasics;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.function.Predicate;
 
 import static org.junit.Assert.fail;
@@ -587,7 +586,7 @@ public final class FrameQuaternionTest extends FrameQuaternionReadOnlyTest<Frame
    public void testConsistencyWithQuaternion() {
       EuclidFrameAPITestTools.FrameTypeBuilder<? extends ReferenceFrameHolder> frameTypeBuilder = (frame, tuple) -> createTuple(frame, ((QuaternionReadOnly)tuple).getX(), ((QuaternionReadOnly)tuple).getY(), ((QuaternionReadOnly)tuple).getZ(), ((QuaternionReadOnly)tuple).getS());
       EuclidFrameAPITestTools.GenericTypeBuilder framelessTypeBuilder = () -> createRandomTuple(random).getGeometryObject();
-      Predicate<Method> methodFilter = m -> !m.getName().equals("hashCode") && Arrays.stream(m.getParameterTypes()).noneMatch(param -> param.getName().contains("4D") || (!param.getName().contains("Quaternion") && param.getName().contains("ReadOnly")));
+      Predicate<Method> methodFilter = m -> !m.getName().equals("hashCode");
       EuclidFrameAPITestTools.assertFrameMethodsOfFrameHolderPreserveFunctionality(frameTypeBuilder, framelessTypeBuilder, methodFilter);
    }
 
@@ -599,5 +598,114 @@ public final class FrameQuaternionTest extends FrameQuaternionReadOnlyTest<Frame
       framelessMethodsToIgnore.put("set", new Class<?>[] {Quaternion.class});
       framelessMethodsToIgnore.put("epsilonEquals", new Class<?>[] {Quaternion.class, Double.TYPE});
       EuclidFrameAPITestTools.assertOverloadingWithFrameObjects(FrameQuaternion.class, Quaternion.class, true, 1, framelessMethodsToIgnore);
+   }
+
+   @Test
+   public void testFrameGeometryObjectFeatures() throws Throwable
+   {
+      FrameGeometryObjectTest<FrameQuaternion, Quaternion> frameGeometryObjectTest = new FrameGeometryObjectTest<FrameQuaternion, Quaternion>()
+      {
+         @Override
+         public Quaternion createEmptyGeometryObject()
+         {
+            return createEmptyTuple().getGeometryObject();
+         }
+
+         @Override
+         public Quaternion createRandomGeometryObject(Random random)
+         {
+            return createRandomTuple(random).getGeometryObject();
+         }
+
+         @Override
+         public FrameQuaternion createEmptyFrameGeometryObject(ReferenceFrame referenceFrame)
+         {
+            return createEmptyTuple(referenceFrame);
+         }
+
+         @Override
+         public FrameQuaternion createFrameGeometryObject(ReferenceFrame referenceFrame, Quaternion geometryObject)
+         {
+            return createTuple(referenceFrame, geometryObject.getX(), geometryObject.getY(), geometryObject.getZ(), geometryObject.getS());
+         }
+
+         @Override
+         public FrameQuaternion createRandomFrameGeometryObject(Random random, ReferenceFrame referenceFrame)
+         {
+            return EuclidFrameRandomTools.generateRandomFrameQuaternion(random, referenceFrame);
+         }
+      };
+
+      for (Method testMethod : frameGeometryObjectTest.getClass().getMethods())
+      {
+         if (!testMethod.getName().startsWith("test"))
+            continue;
+         if (!Modifier.isPublic(testMethod.getModifiers()))
+            continue;
+         if (Modifier.isStatic(testMethod.getModifiers()))
+            continue;
+         // The following are due to normalization altering values during the createTuple() calls
+         if (testMethod.getName().equals("testSetFromReferenceFrame"))
+            continue;
+         if (testMethod.getName().equals("testChangeFrame"))
+            continue;
+         if (testMethod.getName().equals("testGetGeometryObject"))
+            continue;
+
+         try
+         {
+            testMethod.invoke(frameGeometryObjectTest);
+         }
+         catch (InvocationTargetException e)
+         {
+            throw e.getCause();
+         }
+      }
+   }
+
+   @Test
+   public void testQuaternionBasicsFeatures() throws Exception
+   {
+      QuaternionBasicsTest<FrameQuaternion> quaternionBasicsTest = new QuaternionBasicsTest<FrameQuaternion>()
+      {
+         @Override
+         public FrameQuaternion createEmptyTuple()
+         {
+            return new FrameQuaternion();
+         }
+
+         @Override
+         public FrameQuaternion createTuple(double v, double v1, double v2, double v3)
+         {
+            FrameQuaternion ret = new FrameQuaternion(ReferenceFrame.getWorldFrame());
+            ret.setUnsafe(v, v1, v2, v3);
+            return ret;
+         }
+
+         @Override
+         public FrameQuaternion createRandomTuple(Random random)
+         {
+            return EuclidFrameRandomTools.generateRandomFrameQuaternion(random, ReferenceFrame.getWorldFrame());
+         }
+
+         @Override
+         public double getEpsilon()
+         {
+            return 1e-10;
+         }
+      };
+
+      for (Method testMethod : quaternionBasicsTest.getClass().getMethods())
+      {
+         if (!testMethod.getName().startsWith("test"))
+            continue;
+         if (!Modifier.isPublic(testMethod.getModifiers()))
+            continue;
+         if (Modifier.isStatic(testMethod.getModifiers()))
+            continue;
+
+         System.out.println(testMethod.getName());
+         testMethod.invoke(quaternionBasicsTest);
+      }
    }
 }
