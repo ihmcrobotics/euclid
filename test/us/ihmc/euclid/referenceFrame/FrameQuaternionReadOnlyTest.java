9b44da79e1f8ec0d7ef34c4019f4eb048da3cb37
Sylvain Bertrand <sbertrand@ihmc.us>
2017-10-28 15:39:45 -0500

    Added test for epsilonEquals and equals.

--- test/us/ihmc/euclid/referenceFrame/FrameQuaternionReadOnlyTest.java
+++ test/us/ihmc/euclid/referenceFrame/FrameQuaternionReadOnlyTest.java
@@ -1,5 +1,8 @@
 package us.ihmc.euclid.referenceFrame;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.lang.reflect.Method;
 import java.util.Random;
 import java.util.function.Predicate;
@@ -7,7 +10,9 @@ import java.util.function.Predicate;
 import org.junit.Test;
 
 import us.ihmc.euclid.referenceFrame.interfaces.FrameQuaternionReadOnly;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameTuple4DReadOnly;
 import us.ihmc.euclid.referenceFrame.tools.EuclidFrameAPITestTools;
+import us.ihmc.euclid.referenceFrame.tools.EuclidFrameRandomTools;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.euclid.tuple4D.Tuple4DReadOnlyTest;
@@ -74,4 +79,68 @@ public abstract class FrameQuaternionReadOnlyTest<T extends FrameQuaternionReadO
       EuclidFrameAPITestTools.assertMethodsOfReferenceFrameHolderCheckReferenceFrame(frame -> createRandomFrameQuaternion(random, frame), false, true, methodFilter);
       EuclidFrameAPITestTools.assertMethodsOfReferenceFrameHolderCheckReferenceFrame(frame -> createRandom2DFrameQuaternion(random, frame), false, true, methodFilter);
    }
+
+   @Test
+   public void testEpsilonEquals() throws Exception
+   {
+      super.testEpsilonEquals();
+
+      Random random = new Random(621541L);
+      double epsilon = 0.0;
+
+      ReferenceFrame frame1 = ReferenceFrame.getWorldFrame();
+      ReferenceFrame frame2 = EuclidFrameRandomTools.generateRandomReferenceFrame(random);
+
+      double x = random.nextDouble();
+      double y = random.nextDouble();
+      double z = random.nextDouble();
+      double s = random.nextDouble();
+
+      T tuple1 = createFrameQuaternion(frame1, x, y, z, s);
+      T tuple2 = createFrameQuaternion(frame1, x, y, z, s);
+      T tuple3 = createFrameQuaternion(frame2, x, y, z, s);
+      T tuple4 = createFrameQuaternion(frame2, x, y, z, s);
+
+      assertTrue(tuple1.epsilonEquals(tuple2, epsilon));
+      assertFalse(tuple1.epsilonEquals(tuple3, epsilon));
+      assertFalse(tuple3.epsilonEquals(tuple2, epsilon));
+      assertTrue(tuple3.epsilonEquals(tuple4, epsilon));
+
+      assertTrue(tuple1.epsilonEquals((FrameTuple4DReadOnly) tuple2, epsilon));
+      assertFalse(tuple1.epsilonEquals((FrameTuple4DReadOnly) tuple3, epsilon));
+      assertFalse(tuple3.epsilonEquals((FrameTuple4DReadOnly) tuple2, epsilon));
+      assertTrue(tuple3.epsilonEquals((FrameTuple4DReadOnly) tuple4, epsilon));
+   }
+
+   @Test
+   public void testEquals() throws Exception
+   {
+      super.testEquals();
+
+      Random random = new Random(621541L);
+
+      ReferenceFrame frame1 = ReferenceFrame.getWorldFrame();
+      ReferenceFrame frame2 = EuclidFrameRandomTools.generateRandomReferenceFrame(random);
+
+      double x = random.nextDouble();
+      double y = random.nextDouble();
+      double z = random.nextDouble();
+      double s = random.nextDouble();
+
+      T tuple1 = createFrameQuaternion(frame1, x, y, z, s);
+      T tuple2 = createFrameQuaternion(frame1, x, y, z, s);
+      T tuple3 = createFrameQuaternion(frame2, x, y, z, s);
+      T tuple4 = createFrameQuaternion(frame2, x, y, z, s);
+
+      assertTrue(tuple1.equals(tuple2));
+      assertFalse(tuple1.equals(tuple3));
+      assertFalse(tuple3.equals(tuple2));
+      assertTrue(tuple3.equals(tuple4));
+      
+      assertTrue(tuple1.equals((FrameTuple4DReadOnly) tuple2));
+      assertFalse(tuple1.equals((FrameTuple4DReadOnly) tuple3));
+      assertFalse(tuple3.equals((FrameTuple4DReadOnly) tuple2));
+      assertTrue(tuple3.equals((FrameTuple4DReadOnly) tuple4));
+   }
+
 }
