c7822cbb5e43f63b3e850c5acdf2bfb7342eba64
Carson Wilber <cwilber@ihmc.us>
2017-11-06 10:56:42 -0600

    Overload geometricallyEquals in FV3D

--- src/us/ihmc/euclid/referenceFrame/FrameVector3D.java
+++ src/us/ihmc/euclid/referenceFrame/FrameVector3D.java
@@ -260,4 +260,23 @@ public class FrameVector3D extends FrameTuple3D<FrameVector3D, Vector3D> impleme
 
       return Vector3DBasics.super.geometricallyEquals(other, epsilon);
    }
+
+   /**
+    * Compares {@code this} to {@code other} to determine if the two frame vectors
+    * are geometrically similar, i.e. the length of the distance between them is
+    * less than or equal to {@code epsilon}.
+    *
+    * @param other the frame vector to compare to. Not modified.
+    * @param epsilon the tolerance of the comparison.
+    * @throws ReferenceFrameMismatchException if {@code other} is not expressed in the same
+    *            reference frame as {@code this}.
+    * @return {@code true} if the two frame vectors represent the same geometry,
+    *            {@code false} otherwise.
+    */
+   public boolean geometricallyEquals(FrameVector3DReadOnly other, double epsilon)
+   {
+      checkReferenceFrameMatch(other);
+
+      return Vector3DBasics.super.geometricallyEquals(other, epsilon);
+   }
 }
