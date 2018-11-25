05344258ee6003dc90bae1004c344bf58da2248c
Sylvain Bertrand <sbertrand@ihmc.us>
2018-11-25 16:32:14 -0600

    Started filling the polytope tools class.

--- src/shape/java/us/ihmc/euclid/shape/convexPolytope/tools/EuclidPolytopeTools.java
+++ src/shape/java/us/ihmc/euclid/shape/convexPolytope/tools/EuclidPolytopeTools.java
@@ -1,6 +1,160 @@
 package us.ihmc.euclid.shape.convexPolytope.tools;
 
+import static us.ihmc.euclid.tools.EuclidCoreTools.*;
+
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+
 public class EuclidPolytopeTools
 {
+   private static final double ONE_TRILLIONTH = EuclidGeometryTools.ONE_TRILLIONTH;
 
+   public static boolean isPoint3DOnLeftSideOfLine3D(Point3DReadOnly point, Point3DReadOnly firstPointOnLine, Point3DReadOnly secondPointOnLine,
+                                                     Vector3DReadOnly planeNormal)
+   {
+      return isPoint3DOnSideOfLine3D(point, firstPointOnLine, secondPointOnLine, planeNormal, true);
+   }
+
+   public static boolean isPoint3DOnLeftSideOfLine3D(Point3DReadOnly point, Point3DReadOnly pointOnLine, Vector3DReadOnly lineDirection,
+                                                     Vector3DReadOnly planeNormal)
+   {
+      return isPoint3DOnSideOfLine3D(point, pointOnLine, lineDirection, planeNormal, true);
+   }
+
+   public static boolean isPoint3DOnRightSideOfLine3D(Point3DReadOnly point, Point3DReadOnly firstPointOnLine, Point3DReadOnly secondPointOnLine,
+                                                      Vector3DReadOnly planeNormal)
+   {
+      return isPoint3DOnSideOfLine3D(point, firstPointOnLine, secondPointOnLine, planeNormal, false);
+   }
+
+   public static boolean isPoint3DOnRightSideOfLine3D(Point3DReadOnly point, Point3DReadOnly pointOnLine, Vector3DReadOnly lineDirection,
+                                                      Vector3DReadOnly planeNormal)
+   {
+      return isPoint3DOnSideOfLine3D(point, pointOnLine, lineDirection, planeNormal, false);
+   }
+
+   public static boolean isPoint3DOnSideOfLine3D(double pointX, double pointY, double pointZ, double pointOnLineX, double pointOnLineY, double pointOnLineZ,
+                                                 double lineDirectionX, double lineDirectionY, double lineDirectionZ, double planeNormalX, double planeNormalY,
+                                                 double planeNormalZ, boolean testLeftSide)
+   {
+      double dx = pointX - pointOnLineX;
+      double dy = pointY - pointOnLineY;
+      double dz = pointZ - pointOnLineZ;
+      // (lineDirection) X (dx, dy, dz)
+      double crossX = lineDirectionY * dz - lineDirectionZ * dy;
+      double crossY = lineDirectionZ * dx - lineDirectionX * dz;
+      double crossZ = lineDirectionX * dy - lineDirectionY * dx;
+
+      double crossProduct = crossX * planeNormalX + crossY * planeNormalY + crossZ * planeNormalZ;
+
+      if (testLeftSide)
+         return crossProduct > 0.0;
+      else
+         return crossProduct < 0.0;
+   }
+
+   public static boolean isPoint3DOnSideOfLine3D(Point3DReadOnly point, Point3DReadOnly firstPointOnLine, Point3DReadOnly secondPointOnLine,
+                                                 Vector3DReadOnly planeNormal, boolean testLeftSide)
+   {
+      double pointOnLineX = firstPointOnLine.getX();
+      double pointOnLineY = firstPointOnLine.getY();
+      double pointOnLineZ = firstPointOnLine.getZ();
+      double lineDirectionX = secondPointOnLine.getX() - firstPointOnLine.getX();
+      double lineDirectionY = secondPointOnLine.getY() - firstPointOnLine.getY();
+      double lineDirectionZ = secondPointOnLine.getZ() - firstPointOnLine.getZ();
+      return isPoint3DOnSideOfLine3D(point.getX(), point.getY(), point.getZ(), pointOnLineX, pointOnLineY, pointOnLineZ, lineDirectionX, lineDirectionY,
+                                     lineDirectionZ, planeNormal.getX(), planeNormal.getY(), planeNormal.getZ(), testLeftSide);
+   }
+
+   public static boolean isPoint3DOnSideOfLine3D(Point3DReadOnly point, Point3DReadOnly pointOnLine, Vector3DReadOnly lineDirection,
+                                                 Vector3DReadOnly planeNormal, boolean testLeftSide)
+   {
+      return isPoint3DOnSideOfLine3D(point.getX(), point.getY(), point.getZ(), pointOnLine.getX(), pointOnLine.getY(), pointOnLine.getZ(), lineDirection.getX(),
+                                     lineDirection.getY(), lineDirection.getZ(), planeNormal.getX(), planeNormal.getY(), planeNormal.getZ(), testLeftSide);
+   }
+
+   /**
+    * Returns the minimum signed distance between the projection of a 3D point and an infinitely long
+    * 3D line defined by a point and a direction onto a plane of given normal.
+    * <p>
+    * The calculated distance is negative if the query is located on the right side of the line. The
+    * notion of left side is defined as the semi-open space that starts at {@code pointOnLine} and
+    * extends to the direction given by the vector <tt>planeNormal &times; lineDirection</tt> and the
+    * right side starts at {@code pointOnLine} and direction
+    * <tt>-planeNormal &times; lineDirection</tt>. In an intuitive manner, the left side refers to the
+    * side on the left of the line when looking at the line with the plane normal pointing toward you.
+    * </p>
+    * <p>
+    * Note that the position of the plane does not affect the distance separating the query from the
+    * line, so it not required.
+    * </p>
+    * <p>
+    * Edge cases:
+    * <ul>
+    * <li>if {@code lineDirection.length() < }{@value #ONE_TRILLIONTH}, this method returns the
+    * distance between {@code pointOnLine} and the given {@code point}.
+    * </ul>
+    * </p>
+    *
+    * @param pointX x-coordinate of the query.
+    * @param pointY y-coordinate of the query.
+    * @param pointZ z-coordinate of the query.
+    * @param pointOnLineX x-coordinate of a point located on the line.
+    * @param pointOnLineY y-coordinate of a point located on the line.
+    * @param pointOnLineZ z-coordinate of a point located on the line.
+    * @param lineDirectionX x-component of the line direction.
+    * @param lineDirectionY y-component of the line direction.
+    * @param lineDirectionZ y-component of the line direction.
+    * @param planeNormalX x-component of the plane normal.
+    * @param planeNormalY y-component of the plane normal.
+    * @param planeNormalZ z-component of the plane normal.
+    * @return the minimum distance between the projection of the 3D point and line onto the plane. The
+    *         distance is negative if the query is located on the right side of the line.
+    */
+   public static double signedDistanceFromPoint3DToLine3D(double pointX, double pointY, double pointZ, double pointOnLineX, double pointOnLineY,
+                                                          double pointOnLineZ, double lineDirectionX, double lineDirectionY, double lineDirectionZ,
+                                                          double planeNormalX, double planeNormalY, double planeNormalZ)
+   {
+      double directionMagnitude = Math.sqrt(normSquared(lineDirectionX, lineDirectionY, lineDirectionZ));
+
+      double dx = pointOnLineX - pointX;
+      double dy = pointOnLineY - pointY;
+      double dz = pointOnLineZ - pointZ;
+
+      if (directionMagnitude < ONE_TRILLIONTH)
+      {
+         return Math.sqrt(normSquared(dx, dy, dz));
+      }
+      else
+      {
+         // (lineDirection) X (dx, dy, dz)
+         double crossX = lineDirectionY * dz - lineDirectionZ * dy;
+         double crossY = lineDirectionZ * dx - lineDirectionX * dz;
+         double crossZ = lineDirectionX * dy - lineDirectionY * dx;
+
+         return (crossX * planeNormalX + crossY * planeNormalY + crossZ * planeNormalZ) / directionMagnitude;
+      }
+   }
+
+   public static double signedDistanceFromPoint3DToLine3D(Point3DReadOnly point, Point3DReadOnly firstPointOnLine, Point3DReadOnly secondPointOnLine,
+                                                          Vector3DReadOnly planeNormal)
+   {
+      double pointOnLineX = firstPointOnLine.getX();
+      double pointOnLineY = firstPointOnLine.getY();
+      double pointOnLineZ = firstPointOnLine.getZ();
+      double lineDirectionX = secondPointOnLine.getX() - firstPointOnLine.getX();
+      double lineDirectionY = secondPointOnLine.getY() - firstPointOnLine.getY();
+      double lineDirectionZ = secondPointOnLine.getZ() - firstPointOnLine.getZ();
+      return signedDistanceFromPoint3DToLine3D(point.getX(), point.getY(), point.getZ(), pointOnLineX, pointOnLineY, pointOnLineZ, lineDirectionX,
+                                               lineDirectionY, lineDirectionZ, planeNormal.getX(), planeNormal.getY(), planeNormal.getZ());
+   }
+
+   public static double signedDistanceFromPoint3DToLine3D(Point3DReadOnly point, Point3DReadOnly pointOnLine, Vector3DReadOnly lineDirection,
+                                                          Vector3DReadOnly planeNormal)
+   {
+      return signedDistanceFromPoint3DToLine3D(point.getX(), point.getY(), point.getZ(), pointOnLine.getX(), pointOnLine.getY(), pointOnLine.getZ(),
+                                               lineDirection.getX(), lineDirection.getY(), lineDirection.getZ(), planeNormal.getX(), planeNormal.getY(),
+                                               planeNormal.getZ());
+   }
 }
