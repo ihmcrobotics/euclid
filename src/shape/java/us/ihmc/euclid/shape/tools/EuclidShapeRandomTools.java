86924f0498058275873cfb979b90a1b49a104e99
Sylvain Bertrand <sbertrand@ihmc.us>
2019-01-13 16:13:57 -0600

    Added random generators.

--- src/shape/java/us/ihmc/euclid/shape/tools/EuclidShapeRandomTools.java
+++ src/shape/java/us/ihmc/euclid/shape/tools/EuclidShapeRandomTools.java
@@ -12,7 +12,10 @@ import us.ihmc.euclid.shape.Ramp3D;
 import us.ihmc.euclid.shape.Sphere3D;
 import us.ihmc.euclid.shape.Torus3D;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 
 public class EuclidShapeRandomTools
@@ -101,6 +104,79 @@ public class EuclidShapeRandomTools
    {
       return new Torus3D(EuclidCoreRandomTools.nextRigidBodyTransform(random), EuclidCoreRandomTools.nextDouble(random, minRadius, maxRadius),
                          EuclidCoreRandomTools.nextDouble(random, minTubeRadius, maxTubeRadius));
+   }
+
+   public static Point2D nextPoint2DInTriangle(Random random, Point2DReadOnly a, Point2DReadOnly b, Point2DReadOnly c)
+   {
+      return new Point2D(nextPoint3DInTriangle(random, new Point3D(a), new Point3D(b), new Point3D(c)));
+   }
+
+   public static Point3D nextPoint3DInTriangle(Random random, Point3DReadOnly a, Point3DReadOnly b, Point3DReadOnly c)
+   {
+      // Generating random point using the method introduced: http://mathworld.wolfram.com/TrianglePointPicking.html
+      double alpha0 = random.nextDouble();
+      double alpha1 = random.nextDouble();
+
+      if (alpha0 + alpha1 > 1.0)
+      { // The generated would be outside the triangle. Instead of discarding this point, we're folding the parallelogram.
+         alpha0 = 1.0 - alpha0;
+         alpha1 = 1.0 - alpha1;
+      }
+
+      Vector3D v0 = new Vector3D();
+      Vector3D v1 = new Vector3D();
+      v0.sub(b, a);
+      v1.sub(c, a);
+
+      Point3D next = new Point3D(a);
+      next.scaleAdd(alpha0, v0, next);
+      next.scaleAdd(alpha1, v1, next);
+
+      return next;
+   }
+
+   public static Point3D nextPoint3DInTetrahedron(Random random, Point3DReadOnly a, Point3DReadOnly b, Point3DReadOnly c, Point3DReadOnly d)
+   {
+      // Generating random point using the method introduced: http://vcg.isti.cnr.it/publications/papers/rndtetra_a.pdf
+      double s = random.nextDouble();
+      double t = random.nextDouble();
+      double u = random.nextDouble();
+
+      if (s + t > 1.0)
+      {
+         s = 1.0 - s;
+         t = 1.0 - t;
+      }
+
+      if (s + t + u > 1.0)
+      {
+         if (t + u > 1.0)
+         {
+            double tOld = t;
+            t = 1.0 - u;
+            u = 1.0 - s - tOld;
+         }
+         else
+         {
+            double sOld = s;
+            s = 1.0 - t - u;
+            u = sOld + t + u - 1.0;
+         }
+      }
+
+      Vector3D v0 = new Vector3D();
+      Vector3D v1 = new Vector3D();
+      Vector3D v2 = new Vector3D();
+      v0.sub(b, a);
+      v1.sub(c, a);
+      v2.sub(d, a);
+
+      Point3D next = new Point3D(a);
+      next.scaleAdd(s, v0, next);
+      next.scaleAdd(t, v1, next);
+      next.scaleAdd(u, v2, next);
+
+      return next;
    }
 
    // FIXME Generates points that are mostly sitting around the average of the points.
---
+++ src/test/java/us/ihmc/euclid/shape/tools/EuclidShapeRandomToolsTest.java
@@ -0,0 +1,84 @@
+package us.ihmc.euclid.shape.tools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.euclid.tools.RotationMatrixTools;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple2D.Vector2D;
+
+class EuclidShapeRandomToolsTest
+{
+
+   private static final int ITERATIONS = 1000;
+   private static final double EPSILON = 1.0e-12;
+
+   @Test
+   void testNextPoint2DInTriangle()
+   {
+      Random random = new Random(43);
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         Point2D a = EuclidCoreRandomTools.nextPoint2D(random, 5.0);
+         Point2D b = EuclidCoreRandomTools.nextPoint2D(random, 5.0);
+         Point2D c = EuclidCoreRandomTools.nextPoint2D(random, 5.0);
+
+         Point2D nextPoint = EuclidShapeRandomTools.nextPoint2DInTriangle(random, a, b, c);
+         assertTrue(EuclidGeometryTools.isPoint2DInsideTriangleABC(nextPoint, a, b, c));
+      }
+
+      {// Building equilateral triangle to verify identities from: http://mathworld.wolfram.com/TrianglePointPicking.html
+         int numberOfPoints = (int) 1e6;
+         double averageDistanceToCentroid = 0.0;
+         double averageDistanceToA = 0.0;
+         double averageDistanceToB = 0.0;
+         double averageDistanceToC = 0.0;
+
+         for (int i = 0; i < numberOfPoints; i++)
+         {
+            double l = random.nextDouble();
+            Vector2D ab = EuclidCoreRandomTools.nextVector2DWithFixedLength(random, l);
+            Vector2D ac = new Vector2D();
+            RotationMatrixTools.applyYawRotation(Math.PI / 3.0, ab, ac);
+            Point2D a = EuclidCoreRandomTools.nextPoint2D(random, 5.0);
+            Point2D b = new Point2D(a);
+            Point2D c = new Point2D(a);
+            b.add(ab);
+            c.add(ac);
+
+            assertEquals(l, b.distance(c), EPSILON);
+
+            Point2D nextPoint = EuclidShapeRandomTools.nextPoint2DInTriangle(random, a, b, c);
+            assertTrue(EuclidGeometryTools.isPoint2DInsideTriangleABC(nextPoint, a, b, c));
+
+            Point2D centroid = EuclidGeometryTools.averagePoint2Ds(Arrays.asList(a, b, c));
+            averageDistanceToCentroid += centroid.distance(nextPoint) / numberOfPoints;
+            averageDistanceToA += a.distance(nextPoint) / numberOfPoints;
+            averageDistanceToB += a.distance(nextPoint) / numberOfPoints;
+            averageDistanceToC += a.distance(nextPoint) / numberOfPoints;
+         }
+
+         // Somehow the expected distance are exactly 2 times bigger... 
+         double sqrt3 = Math.sqrt(3.0);
+         double expectedDistanceToCentroid = (8.0 * sqrt3 + 3.0 * asinh(sqrt3) + Math.log(2.0 + sqrt3)) / 72.0;
+         assertEquals(0.5 * expectedDistanceToCentroid, averageDistanceToCentroid, 1.0e-4);
+
+         double expectedDistanceToAnyVertex = (4.0 + 3.0 * Math.log(3.0)) / 12.0;
+         assertEquals(0.5 * expectedDistanceToAnyVertex, averageDistanceToA, 1.0e-3);
+         assertEquals(0.5 * expectedDistanceToAnyVertex, averageDistanceToB, 1.0e-3);
+         assertEquals(0.5 * expectedDistanceToAnyVertex, averageDistanceToC, 1.0e-3);
+      }
+   }
+
+   private static double asinh(double x)
+   { // From: http://wwwf.imperial.ac.uk/metric/metric_public/functions_and_graphs/hyperbolic_functions/inverses.html
+      return Math.log(x + Math.sqrt(x * x + 1.0));
+   }
+}
