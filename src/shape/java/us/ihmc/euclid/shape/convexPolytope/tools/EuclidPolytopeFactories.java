1fb2322dcf29bf7025631acc2ec9ceb49e53210d
Sylvain Bertrand <sbertrand@ihmc.us>
2019-04-14 21:52:11 -0500

    Documented EuclidPolytopeFactories.

--- src/shape/java/us/ihmc/euclid/shape/convexPolytope/tools/EuclidPolytopeFactories.java
+++ src/shape/java/us/ihmc/euclid/shape/convexPolytope/tools/EuclidPolytopeFactories.java
@@ -7,6 +7,11 @@ import us.ihmc.euclid.geometry.interfaces.Vertex3DSupplier;
 import us.ihmc.euclid.shape.convexPolytope.ConvexPolytope3D;
 import us.ihmc.euclid.tuple3D.Point3D;
 
+/**
+ * This class provides a set of factories to create primitive shapes as convex polytopes.
+ * 
+ * @author Sylvain Bertrand
+ */
 public class EuclidPolytopeFactories
 {
    private EuclidPolytopeFactories()
@@ -14,6 +19,17 @@ public class EuclidPolytopeFactories
       // Suppresses default constructor, ensuring non-instantiability.
    }
 
+   /**
+    * Discretizes a cone shape and returns a list of vertices representing it.
+    * <p>
+    * The cone has its base center at the origin and its axis of revolution aligned to the z-axis.
+    * </p>
+    * 
+    * @param height the height of the cone.
+    * @param radius the radius of the cone base.
+    * @param numberOfDivisions the number of divisions for discretizing the cone.
+    * @return the cone vertices.
+    */
    public static List<Point3D> newConeVertices(double height, double radius, int numberOfDivisions)
    {
       List<Point3D> vertices = new ArrayList<>();
@@ -28,6 +44,15 @@ public class EuclidPolytopeFactories
       return vertices;
    }
 
+   /**
+    * Creates the list of vertices of a cube.
+    * <p>
+    * The cube is axis-aligned and is centered at the origin.
+    * </p>
+    * 
+    * @param edgeLength the cube edge length.
+    * @return the cube vertices.
+    */
    public static List<Point3D> newCubeVertices(double edgeLength)
    {
       List<Point3D> vertices = new ArrayList<>();
@@ -43,6 +68,17 @@ public class EuclidPolytopeFactories
       return vertices;
    }
 
+   /**
+    * Discretizes a cylinder shape and returns a list of vertices representing it.
+    * <p>
+    * The cylinder is centered at the origin and its axis of revolution is aligned to the z-axis.
+    * </p>
+    * 
+    * @param length the length of the cylinder.
+    * @param radius the radius of the cylinder.
+    * @param numberOfDivisions the number of divisions for discretizing the cylinder.
+    * @return the cylinder vertices.
+    */
    public static List<Point3D> newCylinderVertices(double length, double radius, int numberOfDivisions)
    {
       List<Point3D> vertices = new ArrayList<>();
@@ -63,11 +99,29 @@ public class EuclidPolytopeFactories
       return vertices;
    }
 
+   /**
+    * Creates the list of vertices of a icosahedron.
+    * <p>
+    * The icosahedron is centered at the origin.
+    * </p>
+    * 
+    * @param radius the radius of the circumscribed sphere of the icosahedron.
+    * @return the icosahedron vertices.
+    */
    public static List<Point3D> newIcosahedronVertices(double radius)
    {
       return newIcoSphereVertices(radius, 0);
    }
 
+   /**
+    * Discretizes a sphere using the {@link IcoSphereFactory} and returns a list of vertices
+    * representing it.
+    * 
+    * @param radius the sphere radius.
+    * @param recursionLevel the resolution.
+    * @return the sphere vertices.
+    * @see IcoSphereFactory
+    */
    public static List<Point3D> newIcoSphereVertices(double radius, int recursionLevel)
    {
       List<Point3D> vertices = IcoSphereFactory.newIcoSphere(recursionLevel).getVertices();
@@ -76,6 +130,17 @@ public class EuclidPolytopeFactories
       return vertices;
    }
 
+   /**
+    * Creates the list of vertices of a pyramid.
+    * <p>
+    * The pyramid is axis-aligned and its base is centered at the origin.
+    * </p>
+    *
+    * @param height the height of the pyramid.
+    * @param baseLength the length of the pyramid base.
+    * @param baseWidth the width of the pyramid base.
+    * @return the pyramid vertices.
+    */
    public static List<Point3D> newPyramidVertices(double height, double baseLength, double baseWidth)
    {
       List<Point3D> vertices = new ArrayList<>();
@@ -90,31 +155,90 @@ public class EuclidPolytopeFactories
       return vertices;
    }
 
-   public static ConvexPolytope3D newIcosahedron(double radius)
-   {
-      return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newIcosahedronVertices(radius)));
-   }
-
-   public static ConvexPolytope3D newIcoSphere(double radius, int recursionLevel)
-   {
-      return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newIcoSphereVertices(radius, recursionLevel)));
-   }
-
+   /**
+    * Creates a new convex polytope from discretizing a cone.
+    * <p>
+    * The cone has its base center at the origin and its axis of revolution aligned to the z-axis.
+    * </p>
+    * 
+    * @param height the height of the cone.
+    * @param radius the radius of the cone base.
+    * @param numberOfDivisions the number of divisions for discretizing the cone.
+    * @return the convex polytope representing a cone.
+    */
    public static ConvexPolytope3D newCone(double height, double radius, int numberOfDivisions)
    {
       return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newConeVertices(height, radius, numberOfDivisions)));
    }
 
+   /**
+    * Creates a new convex polytope to represent a cube.
+    * <p>
+    * The cube is axis-aligned and is centered at the origin.
+    * </p>
+    * 
+    * @param edgeLength the cube edge length.
+    * @return the convex polytope representing a cube.
+    */
    public static ConvexPolytope3D newCube(double edgeLength)
    {
       return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newCubeVertices(edgeLength)));
    }
 
+   /**
+    * Creates a new convex polytope from discretizing a cylinder.
+    * <p>
+    * The cylinder is centered at the origin and its axis of revolution is aligned to the z-axis.
+    * </p>
+    * 
+    * @param length the length of the cylinder.
+    * @param radius the radius of the cylinder.
+    * @param numberOfDivisions the number of divisions for discretizing the cylinder.
+    * @return the convex polytope representing a cylinder.
+    */
    public static ConvexPolytope3D newCylinder(double length, double radius, int numberOfDivisions)
    {
       return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newCylinderVertices(length, radius, numberOfDivisions)));
    }
 
+   /**
+    * Creates a new convex polytope to represent a icosahedron.
+    * <p>
+    * The icosahedron is centered at the origin.
+    * </p>
+    * 
+    * @param radius the radius of the circumscribed sphere of the icosahedron.
+    * @return the convex polytope representing a icosahedron.
+    */
+   public static ConvexPolytope3D newIcosahedron(double radius)
+   {
+      return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newIcosahedronVertices(radius)));
+   }
+
+   /**
+    * Creates a new convex polytope to represent a ico-sphere.
+    * 
+    * @param radius the sphere radius.
+    * @param recursionLevel the resolution.
+    * @return the convex polytope representing a ico-sphere.
+    * @see IcoSphereFactory
+    */
+   public static ConvexPolytope3D newIcoSphere(double radius, int recursionLevel)
+   {
+      return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newIcoSphereVertices(radius, recursionLevel)));
+   }
+
+   /**
+    * Creates a new convex polytope to represent a pyramid.
+    * <p>
+    * The pyramid is axis-aligned and its base is centered at the origin.
+    * </p>
+    *
+    * @param height the height of the pyramid.
+    * @param baseLength the length of the pyramid base.
+    * @param baseWidth the width of the pyramid base.
+    * @return the convex polytope representing a pyramid.
+    */
    public static ConvexPolytope3D newPyramid(double height, double baseLength, double baseWidth)
    {
       return new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(newPyramidVertices(height, baseLength, baseWidth)));
