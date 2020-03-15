51f82823e1cdb3be9630eba8c2ffe657f4ce88ab
Sylvain Bertrand <sbertrand@ihmc.us>
2020-03-14 20:15:17 -0500

    Added the 2 basics interfaces.

---
+++ src/frame-shape/java/us/ihmc/euclid/referenceFrame/interfaces/FixedFrameShape3DBasics.java
@@ -0,0 +1,7 @@
+package us.ihmc.euclid.referenceFrame.interfaces;
+
+import us.ihmc.euclid.shape.primitives.interfaces.Shape3DBasics;
+
+public interface FixedFrameShape3DBasics extends Shape3DBasics, FrameShape3DReadOnly
+{
+}
---
+++ src/frame-shape/java/us/ihmc/euclid/referenceFrame/interfaces/FrameShape3DBasics.java
@@ -0,0 +1,6 @@
+package us.ihmc.euclid.referenceFrame.interfaces;
+
+public interface FrameShape3DBasics extends FixedFrameShape3DBasics, FrameChangeable
+{
+
+}
