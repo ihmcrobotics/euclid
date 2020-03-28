3d2bd20567fcf393f49e5fa8dbbcb00686922ad4
Sylvain Bertrand <sbertrand@ihmc.us>
2020-03-28 18:04:10 -0500

    Added test for the new transform optimization in the GJK algorithm.

--- src/test/java/us/ihmc/euclid/shape/collision/GilbertJohnsonKeerthiCollisionDetectorTest.java
+++ src/test/java/us/ihmc/euclid/shape/collision/GilbertJohnsonKeerthiCollisionDetectorTest.java
@@ -18,6 +18,7 @@ import us.ihmc.euclid.geometry.interfaces.Vertex3DSupplier;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryRandomTools;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.shape.collision.gjk.GilbertJohnsonKeerthiCollisionDetector;
+import us.ihmc.euclid.shape.collision.interfaces.SupportingVertexHolder;
 import us.ihmc.euclid.shape.convexPolytope.ConvexPolytope3D;
 import us.ihmc.euclid.shape.convexPolytope.Face3D;
 import us.ihmc.euclid.shape.convexPolytope.HalfEdge3D;
@@ -31,8 +32,10 @@ import us.ihmc.euclid.shape.primitives.Ellipsoid3D;
 import us.ihmc.euclid.shape.primitives.PointShape3D;
 import us.ihmc.euclid.shape.primitives.Ramp3D;
 import us.ihmc.euclid.shape.primitives.Sphere3D;
+import us.ihmc.euclid.shape.primitives.interfaces.Shape3DBasics;
 import us.ihmc.euclid.shape.primitives.interfaces.Shape3DReadOnly;
 import us.ihmc.euclid.shape.tools.EuclidShapeRandomTools;
+import us.ihmc.euclid.shape.tools.EuclidShapeTestTools;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.tools.EuclidCoreTools;
@@ -909,6 +912,30 @@ class GilbertJohnsonKeerthiCollisionDetectorTest
       }, EuclidShapeCollisionTools::evaluateSphere3DRamp3DCollision);
 
       assertAgainstAnalyticalFunction(random, function, distanceMaxEpsilon, positionMaxEpsilon, distanceMeanEpsilon, positionMeanEpsilon);
+   }
+
+   @Test
+   void testShapeTransformOptimization()
+   { // Comparing the method with the Shape3DReadOnly arguments against the one with SupportingVertexHolder argument to validate the transformation optimization done in the first one.
+      Random random = new Random(3466);
+      double distanceEpsilon = 5.0e-6;
+      // When 2 "flat-ish" shapes are closest on their part with low curvature, the closest points can shift along the surface.
+      double pointTangentialEpsilon = 1.0e-2;
+
+      for (int i = 0; i < ITERATIONS; i++)
+      {
+         Shape3DBasics shapeA = EuclidShapeRandomTools.nextConvexShape3D(random);
+         Shape3DBasics shapeB = EuclidShapeRandomTools.nextConvexShape3D(random);
+
+         GilbertJohnsonKeerthiCollisionDetector detector = new GilbertJohnsonKeerthiCollisionDetector();
+         EuclidShape3DCollisionResult expectedResult = detector.evaluateCollision((SupportingVertexHolder) shapeA, (SupportingVertexHolder) shapeB);
+         expectedResult.setShapeA(shapeA);
+         expectedResult.setShapeB(shapeB);
+         EuclidShape3DCollisionResult actualResult = detector.evaluateCollision(shapeA, shapeB);
+
+         EuclidShapeTestTools.assertEuclidShape3DCollisionResultGeometricallyEquals("Iteration "
+               + i, expectedResult, actualResult, distanceEpsilon, pointTangentialEpsilon, 0.0);
+      }
    }
 
    private static <A extends Shape3DReadOnly, B extends Shape3DReadOnly> void assertAgainstAnalyticalFunction(Random random,
