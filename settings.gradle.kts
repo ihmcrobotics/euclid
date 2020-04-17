4218aaad7aa5656e9c2dd04b81d23bf4940ce157
Duncan Calvert <dcalvert@ihmc.us>
2020-04-17 00:54:55 -0500

    Upgrade Gradle scripts to KTS

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+   repositories {
+      maven { url = uri("https://plugins.gradle.org/m2/") }
+      mavenLocal()
+   }
+   dependencies {
+      classpath("us.ihmc:ihmc-build:0.19.7")
+   }
+}
+
+val ihmcSettingsConfigurator = us.ihmc.build.IHMCSettingsConfigurator(settings, logger, extra)
+ihmcSettingsConfigurator.checkRequiredPropertiesAreSet()
+ihmcSettingsConfigurator.configureExtraSourceSets()
+ihmcSettingsConfigurator.findAndIncludeCompositeBuilds()
