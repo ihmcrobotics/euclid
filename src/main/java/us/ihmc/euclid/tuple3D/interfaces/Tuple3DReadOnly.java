cdb6e274e18db765285525a21af3aa0484599c0c
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-25 15:33:49 -0500

    Corrected return type to float for getElement32.

--- src/main/java/us/ihmc/euclid/tuple3D/interfaces/Tuple3DReadOnly.java
+++ src/main/java/us/ihmc/euclid/tuple3D/interfaces/Tuple3DReadOnly.java
@@ -130,7 +130,7 @@ public interface Tuple3DReadOnly
     * @return the value of the component.
     * @throws IndexOutOfBoundsException if {@code index} &notin; [0, 2].
     */
-   default double getElement32(int index)
+   default float getElement32(int index)
    {
       switch (index)
       {
