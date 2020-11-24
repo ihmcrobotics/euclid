71dff077fb7d2d17f6f1392158faabf991b3e5d9
Sylvain Bertrand <sbertrand@ihmc.us>
2020-11-23 20:01:21 -0600

    Added ihmc-commons-testing and ignore pit-reports.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,5 @@ testDependencies {
    api(ihmc.sourceSetProject("frame-shape"))
 
    api("org.ejml:ejml-ddense:0.39")
-   api("org.pitest:pitest:1.5.2")
-   api("org.pitest:pitest-command-line:1.5.2")
+   api("us.ihmc:ihmc-commons-testing:0.30.4")
 }
