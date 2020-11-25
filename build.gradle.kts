2b66281c46f03ee19eb88e96b95be1a8759aa9dd
Duncan Calvert <dcalvert@ihmc.us>
2020-11-25 13:27:50 -0600

    :arrow_up: ihmc-build 0.23.0, ihmc-ci 7.3, ihmc-cd 1.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.22.0"
-   id("us.ihmc.ihmc-ci") version "7.1"
+   id("us.ihmc.ihmc-build") version "0.23.0"
+   id("us.ihmc.ihmc-ci") version "7.3"
 }
 
 ihmc {
